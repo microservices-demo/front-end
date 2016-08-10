83633c7e60bffc6f8edf6592891cc0ec9ff0a910
Carlos León <mail@carlosleon.info>
2016-08-10 13:30:07 +0200

    Replace supertest with chai-http

--- test/helpers_test.js
+++ test/helpers_test.js
@@ -1,54 +1,70 @@
 (function (){
   'use strict';
 
-  var express = require("express");
-  var http = require("http");
-  var request = require("supertest");
-  var expect = require("chai").expect;
-  var helpers = require("../helpers");
-
-  var app = express();
+  var express = require("express")
+    , http = require("http")
+    , chai = require("chai")
+    , chaiHttp = require("chai-http")
+    , expect = chai.expect
+    , helpers = require("../helpers")
+    , app
 
   describe("helpers", function() {
+    before(function() {
+      chai.use(chaiHttp);
+    });
+
+    beforeEach(function() {
+      app = express();
+    });
+
     describe("#respondSuccessBody", function() {
       it("renders the given body with status 200 OK", function(done) {
-        var app = express();
-
         app.use(function(req, res) {
           helpers.respondStatusBody(res, 200, "ayylmao");
         });
 
-        request(app).
+        chai.request(app).
           get("/").
-          expect(200, "ayylmao", done);
+          end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.text).to.equal("ayylmao");
+            done();
+          });
       });
     });
 
     describe("#respondStatusBody", function() {
       it("sets the proper status code & body", function(done) {
-        var app = express();
-
         app.use(function(req, res) {
           helpers.respondStatusBody(res, 201, "foo");
         });
 
-        request(app).
+        chai.request(app).
           get("/").
-          expect(201, "foo", done);
+          end(function(err, res) {
+            expect(res).to.have.status(201);
+            expect(res.text).to.equal("foo");
+            done();
+          });
       });
     });
 
     describe("#respondStatus", function() {
       it("sets the proper status code", function(done) {
-        var app = express();
-
         app.use(function(req, res) {
           helpers.respondStatusBody(res, 404, "");
         });
 
-        request(app).
+        chai.request(app).
           get("/").
-          expect(404, "", done);
+          end(function(err, res) {
+            expect(err).to.not.be.null;
+            expect(err.message).to.equal("Not Found");
+            expect(res).to.have.status(404);
+            expect(res.text).to.equal("");
+            done();
+          });
       });
     });
 
