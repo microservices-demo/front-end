f7cb148d0fce25e36f33b548bbe925ae394fc2b4
Jason Smith <jasonrichardsmith@gmail.com>
2016-08-18 15:45:08 +0200

    added js commas

--- api/endpoints.js
+++ api/endpoints.js
@@ -10,11 +10,11 @@
     addressUrl:    "http://accounts/addresses",
     cardsUrl:      "http://accounts/cards",
     loginUrl:      "http://login/login",
-    registerUrl:   "http://login/register"
-    userCustUrl:   "http://user/customers"
-    userAddrUrl:   "http://user/addresses"
-    userCardUrl:   "http://user/cards"
-    userLogUrl:    "http://user/login"
-    userRegUrl:    "http://user/register"
+    registerUrl:   "http://login/register",
+    userCustUrl:   "http://user/customers",
+    userAddrUrl:   "http://user/addresses",
+    userCardUrl:   "http://user/cards",
+    userLogUrl:    "http://user/login",
+    userRegUrl:    "http://user/register",
   };
 }());
