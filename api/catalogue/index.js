a2f2c6ba0101b662467a6af3b6dfb3e06419ba9b
Ian Crosby <ian.daniel.crosby@gmail.com>
2016-10-02 11:34:03 +0200

    adding endpoint to consume cpu

--- api/catalogue/index.js
+++ api/catalogue/index.js
@@ -20,5 +20,28 @@
     helpers.simpleHttpRequest(endpoints.tagsUrl, res, next);
   });
 
+  app.get("/cycle", function(req, res, next) {
+    var usage_time = 5;
+    if (req.query.time) {
+      console.log("query time passed in");
+      usage_time = parseInt(req.query.time);
+    }
+    blockCpuFor(usage_time * 1000);
+    res.status(200)
+    res.end();
+  });
+
   module.exports = app;
 }());
+
+
+function blockCpuFor(ms) {
+  console.log("Blocking for " + ms + "ms.")
+  var now = new Date().getTime();
+  var result = 0
+  while(true) {
+    result += Math.random() * Math.random();
+    if (new Date().getTime() > now +ms)
+      return;
+  } 
+}
