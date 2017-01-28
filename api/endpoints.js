8042cfe9156d97c9a9ebf3c16c78b48fdb78ed4d
Vishal Lal <vishal.lal@container-solutions.com>
2017-01-28 01:41:46 -0800

    Add optional domain to endpoints

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,15 +1,28 @@
 (function (){
   'use strict';
 
+  const util = require('util');
+
+  var domain = "";
+  process.argv.forEach(function (val, index, array) {
+    var arg = val.split("=");
+    if (arg.length > 1) {
+      if (arg[0] == "--domain") {
+        domain = "." + arg[1];
+        console.log("Setting domain to:", domain);
+      }
+    }
+  });
+
   module.exports = {
-    catalogueUrl:  "http://catalogue",
-    tagsUrl:       "http://catalogue/tags",
-    cartsUrl:      "http://cart/carts",
-    ordersUrl:     "http://orders",
-    customersUrl:  "http://user/customers",
-    addressUrl:    "http://user/addresses",
-    cardsUrl:      "http://user/cards",
-    loginUrl:      "http://user/login",
-    registerUrl:   "http://user/register",
+    catalogueUrl:  util.format("http://catalogue%s", domain),
+    tagsUrl:       util.format("http://catalogue%s/tags", domain),
+    cartsUrl:      util.format("http://cart%s/carts", domain),
+    ordersUrl:     util.format("http://orders%s", domain),
+    customersUrl:  util.format("http://user%s/customers", domain),
+    addressUrl:    util.format("http://user%s/addresses", domain),
+    cardsUrl:      util.format("http://user%s/cards", domain),
+    loginUrl:      util.format("http://user%s/login", domain),
+    registerUrl:   util.format("http://user%s/register", domain),
   };
 }());
