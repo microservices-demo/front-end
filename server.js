99ca61b12c1c6904247a9c8c988f9399c06cc9f7
Jason Smith <jasonrichardsmith@gmail.com>
2017-06-21 11:18:23 +0200

    added ejs templates and package

--- server.js
+++ server.js
@@ -86,3 +86,5 @@ var server = app.listen(process.env.PORT || 8079, function () {
   var port = server.address().port;
   console.log("App now running in %s mode on port %d", app.get("env"), port);
 });
+app.set('view engine', 'ejs')
+
