f3496b8bcd73d91109a8693f6e5dd56695b2751d
Kenaz <kenaz@puppetlabs.com>
2019-09-25 16:26:44 -0700

    Merge

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -z "${NEBULA_EMAIL:-}" ] || [ -z "${NEBULA_PASSWORD:-}" ] || [ -z "${NEBULA_WORKFLOW:-}" ] || [ -z "${DOCKER_TAG:-}" ] || [ "${NO_DOCKER_PUSH:-}" == "yes" ]; then
+    exit 0
+fi
+
+mkdir -p .deploy
+curl -LJ -o .deploy/nebula-cli \
+    -H 'Accept: application/octet-stream' \
+    "https://storage.googleapis.com/nebula-releases/nebula-v3.0.0-linux-amd64"
+chmod +x .deploy/nebula-cli
+
+echo -n "${NEBULA_PASSWORD}" | .deploy/nebula-cli login -e "${NEBULA_EMAIL}" -p
+.deploy/nebula-cli workflow run -n "${NEBULA_WORKFLOW}"
