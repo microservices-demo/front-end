7aec7ad8d268ebf3341ddbdfa9451661542fcdf7
Kenaz <kenaz@puppetlabs.com>
2019-09-26 08:38:50 -0700

    Simplifying Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,3 +13,4 @@ chmod +x .deploy/nebula-cli
 
 echo -n "${NEBULA_PASSWORD}" | .deploy/nebula-cli login -e "${NEBULA_EMAIL}" -p
 .deploy/nebula-cli workflow run -n "${NEBULA_WORKFLOW}"
+.deploy/nebula-cli workflow run list -n "${NEBULA_WORKFLOW}"
