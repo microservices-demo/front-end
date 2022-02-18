4feabeb48bce9ed6d43dad6eac5c93388524e3d5
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-02-18 17:34:40 +1100

    update script

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
+
 COMMIT_SHA=$(git log -1 --format="%H")
-IMAGENAME=nvhoanganh1909/sock-shop-frontend:winstonlogger
+IMAGENAME=nvhoanganh1909/sock-shop-frontend:$1
+
+echo "Building image $IMAGENAME"
+docker buildx build --platform linux/amd64 . -t $IMAGENAME --progress=plain
+
+echo "Pushing $IMAGENAME to dockerhub"
+docker push $IMAGENAME
 
 echo "Updating front-end to use image $IMAGENAME with GIT SHA $COMMIT_SHA"
-
 kubectl set env deployment/front-end NEW_RELIC_METADATA_COMMIT=$COMMIT_SHA --namespace=sock-shop
 kubectl set image deployment/front-end front-end=$IMAGENAME --namespace=sock-shop
+
+echo "watching pods deployment/front-end"
+kubectl get pods --watch
