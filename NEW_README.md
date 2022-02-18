aac4698a9191fa788c8eece6bf2c17ecf195f54d
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-02-18 13:44:15 +1100

    add commit

--- NEW_README.md
+++
@@ -1,2 +0,0 @@
-# this is the new readme file
-- Add Some Content
---
+++ update.sh
@@ -0,0 +1,7 @@
+COMMIT_SHA=$(git log -1 --format="%H")
+IMAGENAME=nvhoanganh1909/sock-shop-frontend:noerror
+
+echo "Updating front-end to use image $IMAGENAME with GIT SHA $COMMIT_SHA"
+
+kubectl set env deployment/front-end NEW_RELIC_METADATA_COMMIT=$COMMIT_SHA --namespace=sock-shop
+kubectl set image deployment/front-end front-end=$IMAGENAME --namespace=sock-shop
