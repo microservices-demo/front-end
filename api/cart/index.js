5ce474959914d1f62f9afdd62b9ac0f5257b1d0c
Anthony Nguyen <nvhoanganh1909@gmail.com>
2022-02-23 09:17:50 +1100

    remove error

--- api/cart/index.js
+++ api/cart/index.js
@@ -120,11 +120,6 @@
       return;
     }
 
-    if (parseInt(req.body.quantity) > 10) {
-      console.log(`Numer of requested item is ${req.body.quantity} which is over the limit.`);
-      throw new Error("Invalid req.body.quantity received");
-    }
-
     var custId = helpers.getCustomerId(req, app.get("env"));
 
     async.waterfall([
